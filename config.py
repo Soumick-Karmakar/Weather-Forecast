149aced249bed2d67a10d48e36b1855cbd93e694
Soumick Karmakar <70286775+Soumick-Karmakar@users.noreply.github.com>
2024-03-18 15:34:32 +0530

    Updated config.py with Weather and City API url

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 
-class Config:
-    API_KEY = 'https://api.weatherbit.io/v2.0/current?key=38140cf0a4304397a1a1273a27fad080&city=Kolkata'
+class Weather_1:
+    API_KEY_1 = 'https://api.weatherbit.io/v2.0/current?key=38140cf0a4304397a1a1273a27fad080&city=Kolkata'
+    DEBUG = False
+
+
+class City_2:
+    API_KEY_2 = 'https://countriesnow.space/api/v0.1/countries/population/cities'
     DEBUG = False
